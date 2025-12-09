f132681931e0d3d0a53ea3879f62aee309114c94
aaaa <aaaa@gmail.com>
2025-12-09 11:18:25 +0900

    リンク修正

--- src/main/java/com/example/app/controller/ReviewController.java
+++ src/main/java/com/example/app/controller/ReviewController.java
@@ -1,7 +1,9 @@
 package com.example.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ReviewController {
 	
 	// レビューする
